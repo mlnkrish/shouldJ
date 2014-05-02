993737b0c2d2cd75c84013ea9bc18254db2c7fbe
Srivatsa Katta <vatsa.katta@gmail.com>
2014-05-02 11:13:12 +0530

    Added endsWith and startsWith in string asserter

--- src/main/java/name/mlnkrishnan/shouldJ/asserter/StringAsserter.java
+++ src/main/java/name/mlnkrishnan/shouldJ/asserter/StringAsserter.java
@@ -3,18 +3,36 @@ package name.mlnkrishnan.shouldJ.asserter;
 import name.mlnkrishnan.shouldJ.failure.ActualValueIsNull;
 import name.mlnkrishnan.shouldJ.failure.ExpectationMismatch;
 
-public class StringAsserter extends ObjectAsserter<String>{
+public class StringAsserter extends ObjectAsserter<String> {
     public StringAsserter(String actual) {
         super(actual);
     }
-    
-    public StringAsserter shouldContain(String expectedSubstring){
-        if(actual == null)
+
+    public StringAsserter shouldContain(String expectedSubstring) {
+        if (actual == null)
             throw new ActualValueIsNull();
-        if(!actual.contains(expectedSubstring)){
-            throw new ExpectationMismatch(String.format("expected <%s> to contain <%s>",actual,expectedSubstring));
+        if (!actual.contains(expectedSubstring)) {
+            throw new ExpectationMismatch(String.format("expected <%s> to contain <%s>", actual, expectedSubstring));
         }
         return this;
     }
-    
+
+
+    public StringAsserter shouldStartWith(String expectedPrefix) {
+        if (actual == null)
+            throw new ActualValueIsNull();
+        if (!actual.startsWith(expectedPrefix)) {
+            throw new ExpectationMismatch(String.format("expected <%s> to start with <%s>", actual, expectedPrefix));
+        }
+        return this;
+    }
+
+    public StringAsserter shouldEndWith(String expectedSuffix) {
+        if (actual == null)
+            throw new ActualValueIsNull();
+        if (!actual.endsWith(expectedSuffix)) {
+            throw new ExpectationMismatch(String.format("expected <%s> to end with <%s>", actual, expectedSuffix));
+        }
+        return this;
+    }
 }
--- src/test/java/name/mlnkrishnan/shouldJ/asserter/StringAsserterTest.java
+++ src/test/java/name/mlnkrishnan/shouldJ/asserter/StringAsserterTest.java
@@ -2,9 +2,9 @@ package name.mlnkrishnan.shouldJ.asserter;
 
 import org.junit.Test;
 
-import static name.mlnkrishnan.shouldJ.ShouldJ.it;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.fail;
+import static name.mlnkrishnan.shouldJ.ShouldJ.it;
 
 public class StringAsserterTest {
 
@@ -19,7 +19,37 @@ public class StringAsserterTest {
             it("abc").shouldContain("cb");
             fail();
         } catch (Throwable e) {
-            assertEquals("expected <abc> to contain <cb>",e.getMessage());
+            assertEquals("expected <abc> to contain <cb>", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldStartWith_Pass() {
+        it("abc-xyz").shouldStartWith("abc-");
+    }
+
+    @Test
+    public void shouldStartWith_Fail() {
+        try {
+            it("abc-xyz").shouldStartWith("ax-");
+            fail();
+        } catch (Throwable e) {
+            assertEquals("expected <abc-xyz> to start with <ax->", e.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldEndWith_Pass() {
+        it("abc-xyz").shouldEndWith("-xyz");
+    }
+
+    @Test
+    public void shouldEndWith_Fail() {
+        try {
+            it("abc-xyz").shouldEndWith("ax-");
+            fail();
+        } catch (Throwable e) {
+            assertEquals("expected <abc-xyz> to end with <ax->", e.getMessage());
         }
     }
 }
